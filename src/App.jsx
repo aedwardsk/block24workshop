c66b187e6e7a875b933816b08c4b637dc1d2cda7
Andy Edwards <andyedwards@outlook.com>
2025-01-29 10:45:30 -0500

    completed puppyList

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import {} from "react";
+import { useState } from "react";
+import { puppyList } from "./data";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  // console.log(puppies);
+  return (
+    <div className='App'>
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
+  );
 }
 
 export default App;
