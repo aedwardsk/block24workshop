89fa3d9a60f581080086a58807e2d5e6a0edb4e1
Andy Edwards <andyedwards@outlook.com>
2025-01-29 13:42:34 -0500

    app seems to be working

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,33 @@ function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
   // console.log(puppies);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <div className='App'>
+      {/* && checks if both are ture/= */}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      {featuredPup && <p>{featuredPup.name}</p>}
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              console.log("puppy id", puppy.id);
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
       })}
     </div>
   );
